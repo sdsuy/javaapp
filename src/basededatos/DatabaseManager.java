769ff3e89e056b46c18e1f7bb40a6ecc595013b2
sdsuy <sdasilva@outlook.es>
2020-08-12 12:03:42 -0300

    Singleton DB

---
+++ src/basededatos/DatabaseManager.java
@@ -0,0 +1,27 @@
+package basededatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+	
+	private static Connection Conexion;
+	
+	private static String CADENA_CONEXION = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+	private static String USUARIO = "JAVA";
+	private static String CLAVE = "JAVA";
+	
+	static {
+		try {
+			Conexion = DriverManager.getConnection(CADENA_CONEXION, USUARIO, CLAVE);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConexion() {
+		return Conexion;
+	}
+
+}
