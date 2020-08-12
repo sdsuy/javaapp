2bbdfce637c0bd8ad78afa21b01c35e241b78dbb
sdsuy <sdasilva@outlook.es>
2020-08-12 12:22:47 -0300

    insert

--- src/basededatos/Funcionalidad.java
+++ src/basededatos/Funcionalidad.java
@@ -1,5 +1,31 @@
 package basededatos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Funcionalidad {
+	
+	private Connection conexion = DatabaseManager.getConexion();
+	
+	// Insertar funcionalidad
+	public void insertarFuncionalidad(int id, String nombre, String descripcion) {
+		String insert = "INSERT INTO funcionalidad (id_funcionalidad, nombre, descripcion) VALUES (?,?,?)";
+		
+		try {
+			PreparedStatement prprdstmt = conexion.prepareStatement(insert);
+			
+			prprdstmt.setInt(1, id);
+			prprdstmt.setString(2, nombre);
+			prprdstmt.setString(3, descripcion);
+			
+			int filasIngresadas = prprdstmt.executeUpdate();
+			System.out.println("Se agregó: " + filasIngresadas + "registros nuevos");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 
 }
