f5a13722df28662225d8e1b862a8c50c0de1e76c
DESKTOP-JQ553RC\Jose Luis Madrid <jose.madrid@estudiantes.utec.edu.uy>
2020-08-12 13:08:26 -0300

    Persona me da error que debo o crear gett. o cambiar a publicos

--- src/basededatos/Persona.java
+++ src/basededatos/Persona.java
@@ -2,7 +2,10 @@ package basededatos;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
 import java.sql.Connection;
 
 public class Persona {
@@ -30,5 +33,33 @@ public class Persona {
 			e.printStackTrace();
 		}
 	}
+	// Seleccionar todos los registros
+		public LinkedList<dto.Persona> seleccionarPersona() {
+			LinkedList<dto.Persona> personas = new LinkedList<>(); 
+			String consulta = "SELECT * FROM PERSONA";
+			try {
+				Statement stmt = conexion.createStatement();
+				ResultSet rset = stmt.executeQuery(consulta);
+				
+				while(rset.next()) {
+					dto.Persona persona = new dto.Persona();
+					persona.id_persona = rset.getInt("ID_PERSONA");
+					persona.documento = rset.getString("DOCUMENTO");
+					persona.apellido1 = rset.getString("APELLIDO1");
+					persona.apellido2 = rset.getString("APELLIDO2");
+					persona.nombre1 = rset.getString("NOMBRE1");
+					persona.nombre2 = rset.getString("NOMBRE2");
+					persona.fecha_nac = rset.getDate("FECHA_NAC");
+					persona.clave = rset.getInt("CLAVE");
+					persona.mail = rset.getString("MAIL");
+					persona.id_rol = rset.getInt("ID_ROL");
+					personas.add(persona);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return personas;
+		}
+
 
 }
