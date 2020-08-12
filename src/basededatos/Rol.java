851c3a31d6542a163dc34253d01c4d303dfeb71a
WildIndex <carlossuarez464@gmail.com>
2020-08-12 12:28:41 -0300

    rol

--- src/basededatos/Rol.java
+++ src/basededatos/Rol.java
@@ -1,5 +1,32 @@
 package basededatos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Rol {
+	
+	private Connection conexion = DatabaseManager.getConexion();
+	
+	//insertar rol
+	
+	public void insertarRol(int id, String nombre, String descripcion) {
+		String insert = "INSERT INTO ROL (ID_ROL, NOMBRE, DESCRIPCION) VALUES (?, ?, ?)";
+		
+		try {
+			PreparedStatement prepare = conexion.prepareStatement(insert);
+			
+			prepare.setInt(1, id);
+			prepare.setString(2, nombre);
+			prepare.setString(3, descripcion);
+			
+			int filas = prepare.executeUpdate();
+			System.out.println("Se agregó: " + filas + "registros nuevos");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 }
