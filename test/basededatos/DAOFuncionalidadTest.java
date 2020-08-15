6b50ac023ff4022713b377d17992eb156de5ba5a
sdsuy <sdasilva@outlook.es>
2020-08-15 09:51:31 -0300

    cambio la forma de acceder a DAO static

--- test/basededatos/DAOFuncionalidadTest.java
+++ test/basededatos/DAOFuncionalidadTest.java
@@ -6,44 +6,42 @@ import java.util.LinkedList;
 
 import org.junit.Test;
 
-public class FuncionalidadTest {
+import dto.Funcionalidad;
+
+public class DAOFuncionalidadTest {
 
 	@Test
 	public void testInsertarFuncionalidad() {
-		Funcionalidad funcionalidad = new Funcionalidad();
-		int filasIngresadas = funcionalidad.insertarFuncionalidad(1, "Nueva", "");
-		funcionalidad.eliminarFuncionalidad(1);
+		int filasIngresadas = DAOFuncionalidad.insertarFuncionalidad(1, "Nueva", "");
+		DAOFuncionalidad.eliminarFuncionalidad(1);
 		assertEquals(1, filasIngresadas);
 	}
 
 	@Test
 	public void testSeleccionarFuncionalidades() {
-		Funcionalidad funcionalidad = new Funcionalidad();
-		funcionalidad.insertarFuncionalidad(1, "Primera", "");
-		funcionalidad.insertarFuncionalidad(2, "Segunda", "");
-		funcionalidad.insertarFuncionalidad(3, "Tercera", "");
-		LinkedList<dto.Funcionalidad> funcionalidades = funcionalidad.seleccionarFuncionalidades();
-		funcionalidad.eliminarFuncionalidad(1);
-		funcionalidad.eliminarFuncionalidad(2);
-		funcionalidad.eliminarFuncionalidad(3);
+		DAOFuncionalidad.insertarFuncionalidad(1, "Primera", "");
+		DAOFuncionalidad.insertarFuncionalidad(2, "Segunda", "");
+		DAOFuncionalidad.insertarFuncionalidad(3, "Tercera", "");
+		LinkedList<Funcionalidad> funcionalidades = DAOFuncionalidad.seleccionarFuncionalidades();
+		DAOFuncionalidad.eliminarFuncionalidad(1);
+		DAOFuncionalidad.eliminarFuncionalidad(2);
+		DAOFuncionalidad.eliminarFuncionalidad(3);
 		assertEquals(3, funcionalidades.size());
 	}
 
 	@Test
 	public void testActualizarFuncionalidad() {
-		Funcionalidad funcionalidad = new Funcionalidad();
-		funcionalidad.insertarFuncionalidad(1, "Vieja", "");
-		funcionalidad.actualizarFuncionalidad(1, "Nueva", "");
-		LinkedList<dto.Funcionalidad> funcionalidades = funcionalidad.seleccionarFuncionalidades();
-		funcionalidad.eliminarFuncionalidad(1);
+		DAOFuncionalidad.insertarFuncionalidad(1, "Vieja", "");
+		DAOFuncionalidad.actualizarFuncionalidad(1, "Nueva", "");
+		LinkedList<Funcionalidad> funcionalidades = DAOFuncionalidad.seleccionarFuncionalidades();
+		DAOFuncionalidad.eliminarFuncionalidad(1);
 		assertTrue(funcionalidades.get(0).nombre.equals("Nueva"));
 	}
 	
 	@Test
 	public void testEliminarFuncionalidad() {
-		Funcionalidad funcionalidad = new Funcionalidad();
-		funcionalidad.insertarFuncionalidad(1, "Borrarme", "");
-		int filasEliminadas = funcionalidad.eliminarFuncionalidad(1);
+		DAOFuncionalidad.insertarFuncionalidad(1, "Borrarme", "");
+		int filasEliminadas = DAOFuncionalidad.eliminarFuncionalidad(1);
 		assertEquals(1, filasEliminadas);
 	}
 
