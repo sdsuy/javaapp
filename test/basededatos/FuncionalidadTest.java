e14f1b9d4fdea7748a9bdc4bff9dd934cbf681ad
sdsuy <sdasilva@outlook.es>
2020-08-15 01:23:30 -0300

    Tests realizados para Funcionalidad en Base de datos

--- test/basededatos/FuncionalidadTest.java
+++ test/basededatos/FuncionalidadTest.java
@@ -2,6 +2,8 @@ package basededatos;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 public class FuncionalidadTest {
@@ -9,20 +11,40 @@ public class FuncionalidadTest {
 	@Test
 	public void testInsertarFuncionalidad() {
 		Funcionalidad funcionalidad = new Funcionalidad();
-		int filasIngresadas = funcionalidad.insertarFuncionalidad(1, "Santiago", "");
+		int filasIngresadas = funcionalidad.insertarFuncionalidad(1, "Nueva", "");
+		funcionalidad.eliminarFuncionalidad(1);
 		assertEquals(1, filasIngresadas);
 	}
 
 	@Test
 	public void testSeleccionarFuncionalidades() {
-//		fail("Not yet implemented");
-		assertTrue(true);
+		Funcionalidad funcionalidad = new Funcionalidad();
+		funcionalidad.insertarFuncionalidad(1, "Primera", "");
+		funcionalidad.insertarFuncionalidad(2, "Segunda", "");
+		funcionalidad.insertarFuncionalidad(3, "Tercera", "");
+		LinkedList<dto.Funcionalidad> funcionalidades = funcionalidad.seleccionarFuncionalidades();
+		funcionalidad.eliminarFuncionalidad(1);
+		funcionalidad.eliminarFuncionalidad(2);
+		funcionalidad.eliminarFuncionalidad(3);
+		assertEquals(3, funcionalidades.size());
 	}
 
 	@Test
 	public void testActualizarFuncionalidad() {
-//		fail("Not yet implemented");
-		assertTrue(true);
+		Funcionalidad funcionalidad = new Funcionalidad();
+		funcionalidad.insertarFuncionalidad(1, "Vieja", "");
+		funcionalidad.actualizarFuncionalidad(1, "Nueva", "");
+		LinkedList<dto.Funcionalidad> funcionalidades = funcionalidad.seleccionarFuncionalidades();
+		funcionalidad.eliminarFuncionalidad(1);
+		assertTrue(funcionalidades.get(0).nombre.equals("Nueva"));
+	}
+	
+	@Test
+	public void testEliminarFuncionalidad() {
+		Funcionalidad funcionalidad = new Funcionalidad();
+		funcionalidad.insertarFuncionalidad(1, "Borrarme", "");
+		int filasEliminadas = funcionalidad.eliminarFuncionalidad(1);
+		assertEquals(1, filasEliminadas);
 	}
 
 }
