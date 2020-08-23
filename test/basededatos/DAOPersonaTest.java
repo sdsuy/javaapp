c5763c08a995074ae37f72c2898ec06a1f951594
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-08-23 18:25:02 -0300

    Creao el Test Unitario de DAOPersona

---
+++ test/basededatos/DAOPersonaTest.java
@@ -0,0 +1,21 @@
+package basededatos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DAOPersonaTest {
+
+	@Test
+	public void testNuevaPersona() {
+		// fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+	@Test
+	public void testSeleccionarPersona() {
+		// fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
