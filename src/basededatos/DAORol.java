b6813d1b1dc2fe226bef917178a1297e37da6622
WildIndex <carlossuarez464@gmail.com>
2020-08-23 23:34:32 -0300

    Pronta DAORol

--- src/basededatos/DAORol.java
+++ src/basededatos/DAORol.java
@@ -9,46 +9,93 @@ import java.util.LinkedList;
 
 public class DAORol {
 	
-	private Connection conexion = DatabaseManager.getConexion();
+	private static Connection conexion = DatabaseManager.getConexion();
+	
+	private static final String INSERT = "INSERT INTO ROL (ID_ROL, nombre, descripcion) VALUES (?,?,?)";
+	private static final String SELECT = "SELECT * FROM ROL";
+	private static final String UPDATE = "UPDATE ROL SET nombre=?, descripcion=? WHERE ID_ROL=?";
+	private static final String DELETE = "DELETE FROM ROL WHERE ID_ROL=?";
 	
 	//insertar rol
 	
 	public void insertarRol(int id, String nombre, String descripcion) {
-		String insert = "INSERT INTO ROL (ID_ROL, NOMBRE, DESCRIPCION) VALUES (?, ?, ?)";
 		
 		try {
-			PreparedStatement prepare = conexion.prepareStatement(insert);
 			
-			prepare.setInt(1, id);
-			prepare.setString(2, nombre);
-			prepare.setString(3, descripcion);
+			PreparedStatement sentencia = conexion.prepareStatement(INSERT);
 			
-			int filas = prepare.executeUpdate();
-			System.out.println("Se agregó: " + filas + "registros nuevos");
+			sentencia.setInt(1, id);
+			sentencia.setString(2, nombre);
+			sentencia.setString(3, descripcion);
+			
+			int filasInsert = sentencia.executeUpdate();
+			System.out.println("Se agregaron: " + filasInsert + "registros nuevos");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	// Seleccionar todos los registros
-		public LinkedList<dto.Rol> seleccionarFuncionalidades() {
+		public LinkedList<dto.Rol> seleccionarRol() {
+			
 			LinkedList<dto.Rol> roles = new LinkedList<>(); 
-			String consulta = "SELECT * FROM funcionalidad";
+			
 			try {
-				Statement stmt = conexion.createStatement();
-				ResultSet rset = stmt.executeQuery(consulta);
+				Statement sentencia = conexion.createStatement();
+				ResultSet resultado = sentencia.executeQuery(SELECT);
 				
-				while(rset.next()) {
+				while(resultado.next()) {
 					dto.Rol rol = new dto.Rol();
-					rol.setId(rset.getInt("id_funcionalidad"));
-					rol.setNombre(rset.getString("nombre"));
-					rol.setDescripcion(rset.getString("descripcion"));
+					rol.id = resultado.getInt("id_funcionalidad");
+					rol.nombre = resultado.getString("nombre");
+					rol.descripcion = resultado.getString("descripcion");
 					roles.add(rol);
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 			return roles;
+		}
+		
+		//Actualizar un regstro
+		public static void actualizarRol(int id, String nombre, String descripcion) {
+			
+			try {
+				PreparedStatement sentencia = conexion.prepareStatement(UPDATE);
+				
+				sentencia.setString(1, nombre);
+				sentencia.setString(2, descripcion);
+				sentencia.setInt(3, id);
+				
+				int filasUpdate = sentencia.executeUpdate();
+				
+				System.out.println("Se actualizaron: " + filasUpdate + " registros");
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+			
+		}
+		
+		// Eliminar un registro
+		public static int eliminarRol(int id) {
+			
+			int filasEliminadas = 0;
+			
+			try {
+				PreparedStatement sentencia = conexion.prepareStatement(DELETE);
+				
+				sentencia.setInt(1, id);
+				
+				filasEliminadas = sentencia.executeUpdate();
+				
+				System.out.println("Se eliminaron: " + filasEliminadas + " registros");
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return filasEliminadas;
 		}
 	
 	
