81886af23a87e744520c672564ac1f2187b8872f
sdsuy <sdasilva@outlook.es>
2020-08-24 14:21:59 -0300

    BO de Funcionalidad

--- src/dto/FuncionalidadBO.java
+++ src/dto/FuncionalidadBO.java
@@ -1,5 +1,68 @@
 package dto;
 
+import java.util.LinkedList;
+
+import basededatos.DAOFuncionalidad;
+
 public class FuncionalidadBO {
+	
+	private LinkedList<FuncionalidadVO> funcionalidades;
+	
+	/**
+	 * 
+	 * Constructor que crea un linkedlist de funcionalidades vacio
+	 */
+	public FuncionalidadBO() {
+		this.funcionalidades = new LinkedList<>();
+	}
+
+	/**
+	 * 
+	 * Agrego una funcionalidad
+	 * @param funcionalidad
+	 * @return
+	 */
+	public boolean agregarFuncionalidad(FuncionalidadVO funcionalidad) {
+		return DAOFuncionalidad.insertarFuncionalidad(funcionalidad);
+	}
+	
+	/**
+	 * 
+	 * Cargo las funcionalidades
+	 */
+	public void obtenerFuncionalidades() {
+		funcionalidades = DAOFuncionalidad.seleccionarFuncionalidades();
+	}
+	
+	/**
+	 * 
+	 * Actualizo una funcionalidad
+	 * @param funcionalidad
+	 * @return
+	 */
+	public boolean actualizarFuncionalidad(FuncionalidadVO funcionalidad) {
+		return DAOFuncionalidad.actualizarFuncionalidad(funcionalidad);
+	}
+	
+	/**
+	 * 
+	 * Elimino una funcionalidad
+	 * @param id
+	 * @return
+	 */
+	public boolean eliminarFuncionalidad(int id) {
+		return DAOFuncionalidad.eliminarFuncionalidad(id);
+	}
+
+	/**
+	 * 
+	 * Getter del linkedlist funcionalidades
+	 * @return
+	 */
+	public LinkedList<FuncionalidadVO> getFuncionalidades() {
+		return funcionalidades;
+	}
+	
+	
 
 }
