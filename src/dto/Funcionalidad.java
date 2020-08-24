b946b2dbd56c5fe23ca8c47dc62636f89dc4a9e3
sdsuy <sdasilva@outlook.es>
2020-08-23 23:08:06 -0300

    Rol y Funcionalidad a private los atributos

--- src/dto/Funcionalidad.java
+++ src/dto/Funcionalidad.java
@@ -1,9 +1,56 @@
 package dto;
 
+import java.util.LinkedList;
+
 public class Funcionalidad {
 	
-	public int id;
-	public String nombre;
-	public String descripcion;
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private LinkedList<Rol> roles;
+	
+	public Funcionalidad() {
+		super();
+	}
+
+	public Funcionalidad(int id, String nombre, String descripcion, LinkedList<Rol> roles) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.roles = roles;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public LinkedList<Rol> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(LinkedList<Rol> roles) {
+		this.roles = roles;
+	}
 
 }
--- src/dto/Rol.java
+++ src/dto/Rol.java
@@ -1,9 +1,56 @@
 package dto;
 
+import java.util.LinkedList;
+
 public class Rol {
 	
-	public int id;
-	public String nombre;
-	public String descripcion;
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private LinkedList<Funcionalidad> funcionalidades;
+	
+	public Rol() {
+		super();
+	}
+
+	public Rol(int id, String nombre, String descripcion, LinkedList<Funcionalidad> funcionalidades) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.funcionalidades = funcionalidades;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public LinkedList<Funcionalidad> getFuncionalidades() {
+		return funcionalidades;
+	}
+
+	public void setFuncionalidades(LinkedList<Funcionalidad> funcionalidades) {
+		this.funcionalidades = funcionalidades;
+	}
 
 }
