b9c470448c8a9a0badb24c35dc602352112a3a08
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-08-24 13:27:21 -0300

    Creada la ventana Rol

--- src/presentacion/Rol.java
+++ src/presentacion/Rol.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JButton;
 
 public class Rol extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textNombre;
+	private JTextField textDescripcion;
 
 	/**
 	 * Launch the application.
@@ -35,8 +41,49 @@ public class Rol extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textNombre = new JTextField();
+		textNombre.setBounds(18, 51, 130, 26);
+		contentPane.add(textNombre);
+		textNombre.setColumns(10);
+		
+		textDescripcion = new JTextField();
+		textDescripcion.setColumns(10);
+		textDescripcion.setBounds(18, 119, 130, 26);
+		contentPane.add(textDescripcion);
+		
+		JLabel lblNewLabel = new JLabel("Nombre");
+		lblNewLabel.setBounds(20, 27, 61, 16);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblDescripcion = new JLabel("Descripcion");
+		lblDescripcion.setBounds(18, 91, 95, 16);
+		contentPane.add(lblDescripcion);
+		
+		JList listFuncionalidades = new JList();
+		listFuncionalidades.setBounds(234, 22, 197, 235);
+		contentPane.add(listFuncionalidades);
+		
+		JButton btnModificar = new JButton("Modificar");
+		btnModificar.setBounds(6, 193, 117, 29);
+		contentPane.add(btnModificar);
+		
+		JButton btnListar = new JButton("Listar");
+		btnListar.setBounds(38, 234, 153, 29);
+		contentPane.add(btnListar);
+		
+		JButton btnAlta = new JButton("Alta");
+		btnAlta.setBounds(6, 152, 117, 29);
+		contentPane.add(btnAlta);
+		
+		JButton btnBaja = new JButton("Baja");
+		btnBaja.setBounds(117, 152, 117, 29);
+		contentPane.add(btnBaja);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		btnEliminar.setBounds(117, 193, 117, 29);
+		contentPane.add(btnEliminar);
 	}
-
 }
