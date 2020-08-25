e0fa40e6648d0b48f6791b0b0afb89e047b7aee9
WildIndex <carlossuarez464@gmail.com>
2020-08-24 22:15:48 -0300

    Pronta PersonaBO

--- src/dto/PersonaBO.java
+++ src/dto/PersonaBO.java
@@ -1,5 +1,63 @@
 package dto;
 
+import java.util.LinkedList;
+
+import basededatos.DAOPersona;
+import basededatos.DAORol;
+
 public class PersonaBO {
+	
+private LinkedList<PersonaVO> personas;
+	
+	//Constructor que crea un linkedlist de personas vacio
+	
+	public PersonaBO() {
+		this.personas = new LinkedList<>();
+	}
+
+	/**
+	 * 
+	 * Agrego una persona
+	 * @param persona
+	 * @return
+	 */
+	public boolean agregarPersona(PersonaVO persona) {
+		return DAOPersona.nuevaPersona(persona);
+	}
+	
+	 //Cargo las personas
+	 
+	public void obtenerPersonas() {
+		personas = DAOPersona.seleccionarPersona();
+	}
+	
+	/**
+	 * 
+	 * Actualizo una persona
+	 * @param persona
+	 * @return
+	 */
+	public boolean actualizarPersona(PersonaVO persona) {
+		return DAOPersona.modificarPersona(persona);
+	}
+	
+	/**
+	 * 
+	 * Elimino una persona
+	 * @param idPersona
+	 * @return
+	 */
+	public boolean eliminarPersona(int idPersona) {
+		return DAOPersona.eliminarPersona(idPersona);
+	}
+
+	/**
+	 * 
+	 * Getter del linkedlist personas
+	 * @return
+	 */
+	public LinkedList<PersonaVO> getPersonas() {
+		return personas;
+	}
 
 }
