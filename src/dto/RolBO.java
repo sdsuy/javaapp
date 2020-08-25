79616b858c653ce83bcfc9e12524d3a12c2aede6
WildIndex <carlossuarez464@gmail.com>
2020-08-24 21:55:23 -0300

    Lista RolBO

--- src/dto/RolBO.java
+++ src/dto/RolBO.java
@@ -1,5 +1,66 @@
 package dto;
 
+import java.util.LinkedList;
+
+import basededatos.DAORol;
+
 public class RolBO {
+	
+private LinkedList<RolVO> roles;
+	
+	/**
+	 * 
+	 * Constructor que crea un linkedlist de roles vacio
+	 */
+	public RolBO() {
+		this.roles = new LinkedList<>();
+	}
+
+	/**
+	 * 
+	 * Agrego un rol
+	 * @param rol
+	 * @return
+	 */
+	public boolean agregarRol(RolVO rol) {
+		return DAORol.insertarRol(rol);
+	}
+	
+	/**
+	 * 
+	 * Cargo los roles
+	 */
+	public void obtenerRoles() {
+		roles = DAORol.seleccionarRoles();
+	}
+	
+	/**
+	 * 
+	 * Actualizo un rol
+	 * @param rol
+	 * @return
+	 */
+	public boolean actualizarRol(RolVO rol) {
+		return DAORol.actualizarRol(rol);
+	}
+	
+	/**
+	 * 
+	 * Elimino un rol
+	 * @param idRol
+	 * @return
+	 */
+	public boolean eliminarRol(int idRol) {
+		return DAORol.eliminarRol(idRol);
+	}
+
+	/**
+	 * 
+	 * Getter del linkedlist roles
+	 * @return
+	 */
+	public LinkedList<RolVO> getRoles() {
+		return roles;
+	}
 
 }
