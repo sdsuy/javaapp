1805f53b5dc8293d30656e329cee850ba5581865
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-08-25 23:55:10 -0300

    Eliminado id, id rol y clave

--- src/presentacion/Persona.java
+++ src/presentacion/Persona.java
@@ -304,7 +304,7 @@ public class Persona extends JFrame {
 						try {
 							// Actualizo la lista de personas
 							persona.obtenerPersonas();
-							String[] columnas = { "ID_PERSONA", "DOCUMENTO", "APELLIDO1", "APELLIDO2", "NOMBRE1", "NOMBRE2", "FECHA_NAC", "CLAVE", "MAIL", "ID_Rol" };
+							String[] columnas = {"DOCUMENTO", "APELLIDO1", "APELLIDO2", "NOMBRE1", "NOMBRE2", "FECHA_NAC", "MAIL"};
 							
 							Listado frame = new Listado(persona.getPersonas(), columnas);
 							frame.setVisible(true);
