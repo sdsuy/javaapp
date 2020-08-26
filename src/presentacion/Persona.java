364fd1ca4d571af1d2dc4e6133cc2ef0b7728bb1
sdsuy <sdasilva@outlook.es>
2020-08-26 05:33:45 -0300

    Al modificar no se incuia el Rol ni verificaba las claves sean iguales

--- src/presentacion/Persona.java
+++ src/presentacion/Persona.java
@@ -236,6 +236,11 @@ public class Persona extends JFrame {
 				}
 				
 				else {
+					// tomo el nombre del rol del comboBox y lo paso a rolaux desde el BO luego de buscarlo
+					String nombreRol = cmbBoxRol.getSelectedItem().toString();
+					rol.buscarRol(nombreRol);
+					RolVO rolaux = rol.getRol();
+					
 					PersonaVO peraux = new PersonaVO();
 					persona.buscarPersona(textDocumento.getText());
 					peraux = persona.getPersona();
@@ -248,19 +253,25 @@ public class Persona extends JFrame {
 					peraux.setFecha_nac(sqldate);
 					peraux.setClave(passwordFieldpaswordClave.getText());
 					peraux.setMail(textMail.getText());
-					
-					
-					boolean resultado = persona.actualizarPersona(peraux);
-					
-					if (resultado) {
-						limpiarCampos();
+					peraux.setRol(rolaux);
+					String repClave = passwordFieldRepetirClave.getText();
+					if(repClave.equals(passwordFieldpaswordClave.getText())) {
+						boolean resultado = persona.actualizarPersona(peraux);
 						
-						JOptionPane.showMessageDialog(null, "La operacion se realizo con exito","Correcto",JOptionPane.INFORMATION_MESSAGE);
+						if (resultado) {
+							limpiarCampos();
+							
+							JOptionPane.showMessageDialog(null, "La operacion se realizo con exito","Correcto",JOptionPane.INFORMATION_MESSAGE);
+						}
+						
+						else {
+							JOptionPane.showMessageDialog(null, "Se produjo un error","Error",JOptionPane.ERROR_MESSAGE);
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "Clave no coincide","Error",JOptionPane.ERROR_MESSAGE);
 					}
 					
-					else {
-						JOptionPane.showMessageDialog(null, "Se produjo un error","Error",JOptionPane.ERROR_MESSAGE);
-					}
+					
 					
 					
 				}
