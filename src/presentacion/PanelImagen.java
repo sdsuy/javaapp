d235f56bcabae236fcbdd1b7d81a8c4356f624ac
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-08-27 17:22:57 -0300

    Creada la clase Imagen

---
+++ src/presentacion/PanelImagen.java
@@ -0,0 +1,22 @@
+package presentacion;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+
+public class PanelImagen extends javax.swing.JPanel {
+	
+	public PanelImagen() {
+		this.setSize(400,280);
+	}
+	@Override
+	public void paintComponent(Graphics g) {
+		Dimension tam = getSize();
+		ImageIcon imagen = new ImageIcon(getClass().getResource("/imagen/STIGMA.jpeg"));
+		g.drawImage(imagen.getImage(), 0, 0, tam.width, tam.height, null);
+		setOpaque(false);
+		super.paintComponent(g);
+	}
+
+}
